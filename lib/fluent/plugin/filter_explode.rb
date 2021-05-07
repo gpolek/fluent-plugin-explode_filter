d703ef5b9268e2342986a8a13adac58ca97637c7
Grzegorz Polek <gpolek@g2a.com>
2021-05-07 10:53:36 +0200

    feat: join mutate

--- lib/fluent/plugin/filter_explode.rb
+++ lib/fluent/plugin/filter_explode.rb
@@ -6,8 +6,8 @@ module Fluent
     Fluent::Plugin.register_filter('explode', self)
 
     def filter(tag, time, record)
-      event = Fluent::PluginMixin::MutateEvent.new(record, expand_nesting:true)
-      event_data = Fluent::PluginMixin::MutateEvent.new({}, expand_nesting:true)
+      event = Fluent::Plugin::Mixin::MutateEvent.new(record, expand_nesting:true)
+      event_data = Fluent::Plugin::Mixin::MutateEvent.new({}, expand_nesting:true)
 
       event.keys.each do |key|
         event_data.set(key, event.get(key))
