9feba3217e7887d6bfcb36cce7c8d1196c4df1a1
Grzegorz Polek <gpolek@g2a.com>
2021-05-07 11:49:20 +0200

    feat: tests

--- lib/fluent/plugin/filter_explode.rb
+++ lib/fluent/plugin/filter_explode.rb
@@ -14,7 +14,7 @@ module Fluent
     def iterate(event)
       event_data = Fluent::Plugin::Mixin::MutateEvent.new({}, expand_nesting:true)
       event.each do |key, value|
-        value.is_a?(Hash) ? event_data.set(key, iterate(Fluent::Plugin::Mixin::MutateEvent.new(event.get(key), expand_nesting:true))) : event_data.set(key, event.get(key))
+        value.is_a?(Hash) ? event_data.set(key, iterate(Fluent::Plugin::Mixin::MutateEvent.new(event.get(key), expand_nesting:true))) : event_data.set(key, value)
       end
       return event_data
     end
---
+++ test/test_filter_explode_filter.rb
@@ -0,0 +1,44 @@
+# coding: utf-8
+require 'fluent/test'
+require 'fluent/test/driver/filter'
+require 'fluent/plugin/filter_explode'
+require 'test/unit'
+
+class ExplodeFilterTest < Test::Unit::TestCase
+  def setup
+    Fluent::Test.setup
+    @tag = 'test.tag'
+  end
+
+  CONFIG = %q!
+    
+  !
+
+  def create_driver(conf = CONFIG)
+    Fluent::Test::Driver::Filter.new(Fluent::ExplodeFilter).configure(conf)
+  end
+
+  def get_hostname
+    require 'socket'
+    Socket.gethostname.chomp
+  end
+
+  def test_configure
+    d = create_driver
+    # map = d.instance.instance_variable_get(:@map)
+
+    # map.each_pair { |k, v|
+    #   assert v.is_a?(Fluent::ExplodeFilter)
+    # }
+  end
+
+  def test_format
+    d = create_driver
+    
+    d.run(default_tag: @tag) do
+      d.feed("k" => 'v1', "foo.bar" => 'v2', "log.test" => 'v3')
+    end
+
+    assert_equal [{"k" => 'v1', "foo" => { "bar" => 'v2' }, "log" => { "test" => 'v3' }}], d.filtered.map(&:last)
+  end
+end
